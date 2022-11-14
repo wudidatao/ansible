d5aa71de953183b2aaf5e29289cb25e14b3a2b89
wudidatao <82302925@qq.com>
2022-11-14 17:52:54 +0800

    优化mysql代码

--- mysql/docker/8.0.25/ansible_mysql_docker.sh
+++ mysql/docker/8.0.25/ansible_mysql_docker.sh
@@ -3,40 +3,18 @@
 . ./var.sh
 
 if [ $1 = "push" ];then
-  ansible $harbor_ip -m script -a "mysql_docker.sh push $harbor_ip $mysql_version"
+  ansible $harbor_ip -m script -a "mysql_docker.sh $1 $harbor_ip $harbor_library $mysql_version"
 fi
 
 if [ $1 = "pull" ];then
-  ansible $mysql_ip -m script -a "mysql_docker.sh pull $harbor_ip $mysql_version"
+  ansible $mysql_ip -m script -a "mysql_docker.sh $1 $harbor_ip $harbor_library $mysql_version"
 fi
 
-if [ $1 == "init" ];then
-  ansible $mysql_ip -m script -a "mysql_docker_change_conf.sh init $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-  ansible $mysql_ip -m script -a "mysql_docker.sh init $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-fi
-
-if [ $1 == "create" ];then
-  ansible $mysql_ip -m script -a "mysql_docker_change_conf.sh create $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-  ansible $mysql_ip -m script -a "mysql_docker.sh create $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode" 
-fi
-
-if [ $1 == "clean" ];then
-  ansible $mysql_ip -m script -a "mysql_docker.sh clean $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-fi
-
-if [ $1 == "status" ];then
-  ansible $mysql_ip -m script -a "mysql_docker.sh status $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-fi
-
-if [ $1 == "start" ];then
-  ansible $mysql_ip -m script -a "mysql_docker.sh start $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-fi
-
-if [ $1 == "stop" ];then
-  ansible $mysql_ip -m script -a "mysql_docker.sh stop $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
+if [ $1 == "init" ] || [ $1 == "create" ] || [ $1 == "clean" ] || [ $1 == "delete" ] || [ $1 == "status" ] || [ $1 == "start" ] || [ $1 == "stop" ] || [ $1 == "restart" ];then
+  ansible $mysql_ip -m script -a "mysql_docker.sh $1 $harbor_ip $harbor_library $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
 fi
 
 if [ $1 == "upgrade" ];then
-  ansible $mysql_ip -m script -a "mysql_docker_change_conf.sh upgrade $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
-  ansible $mysql_ip -m script -a "mysql_docker.sh upgrade $harbor_ip $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
+  ansible $mysql_ip -m script -a "mysql_docker.sh $1 $harbor_ip $harbor_library $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode $mysql_upgrade_version"
 fi
+
--- mysql/docker/8.0.25/mysql_docker.sh
+++ mysql/docker/8.0.25/mysql_docker.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 harbor_ip=$2
-mysql_version=$3
-mysql_port=$4
-mysql_home=$5
-mysql_path=$6
-mysql_data=$7
-mysql_conf=$8
-mysql_logs=$9
-mysql_user=${10}
-mysql_description=${11}
-mysql_container_name=${12}
-network_mode=${13}
-harbor_library=mysql
+harbor_library=$3
+mysql_version=$4
 docker_image=$harbor_library:$mysql_version
 
 if [ $1 == "push" ];then
@@ -28,8 +18,37 @@ if [ $1 == "pull" ];then
   docker rmi $harbor_ip/$harbor_library/$docker_image
 fi
 
+mysql_port=$5
+mysql_home=$6
+mysql_path=$7
+mysql_data=$8
+mysql_conf=$9
+mysql_logs=${10}
+mysql_user=${11}
+mysql_description=${12}
+mysql_container_name=${13}
+network_mode=${14}
+mysql_upgrade_version=${15}
+
 if [ $1 == "init" ];then
-  docker run -d -p $mysql_port:$mysql_port --net $network_mode -v /etc/localtime:/etc/localtime --name $mysql_container_name $docker_image
+
+   #创建初始化用户
+  if id -u $mysql_user >/dev/null 2>&1;then
+    echo $mysql_user"用户已创建过"
+  else
+    sudo /usr/sbin/groupadd $mysql_user -g 999 -o
+    sudo /usr/sbin/useradd $mysql_user -u 999 -g $mysql_user -s /sbin/nologin -o
+    echo $mysql_user"用户已新创建"
+  fi
+
+  #初始化目录
+  mkdir -p $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs
+
+  #权限调整
+  sudo chown -R $mysql_user:$mysql_user $mysql_home
+  
+  #初始化容器
+  docker run -d -p $mysql_port:$mysql_port --net $network_mode -v /etc/localtime:/etc/localtime -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --name $mysql_container_name $docker_image
 
   #复制配置
   sudo docker cp $mysql_container_name:/etc/mysql/conf.d $mysql_conf/
@@ -40,11 +59,15 @@ fi
 
 if [ $1 == "clean" ];then
   docker rm -vf $mysql_container_name
+fi
+
+if [ $1 == "delete" ];then
+  docker rm -vf $mysql_container_name
   rm $mysql_path -rf
 fi
 
 if [ $1 == "create" ];then
-  docker run -d -p $mysql_port:$mysql_port --net $network_mode -v $mysql_conf/conf.d:/etc/mysql/conf.d -v $mysql_data:/var/lib/mysql  -v $mysql_logs:/var/log/mysql -v /etc/localtime:/etc/localtime -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --name $mysql_container_name $docker_image
+  docker run -d -p $mysql_port:$mysql_port --net $network_mode -v $mysql_conf/conf.d:/etc/mysql/conf.d -v $mysql_data:/var/lib/mysql -v $mysql_logs:/var/log/mysql -v /etc/localtime:/etc/localtime -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --name $mysql_container_name $docker_image
 fi
 
 if [ $1 == "status" ];then
@@ -62,6 +85,11 @@ if [ $1 == "stop" ];then
 fi
 
 if [ $1 == "upgrade" ] ;then
-  docker stop ${mysql_description}-1.22.0-${mysql_port}
-  docker run -d --privileged -p $mysql_port:$mysql_port --net $network_mode -v $mysql_conf/mysql.conf:/etc/mysql/mysql.conf -v $mysql_data/html:/usr/share/mysql/html  -v $mysql_logs:/var/log/mysql -v /etc/localtime:/etc/localtime --name $mysql_container_name $docker_image
+  docker stop $mysql_container_name
+  mkdir $mysql_home/$mysql_upgrade_version-${mysql_port}
+  chown -R $mysql_user:$mysql_user $mysql_home/$mysql_upgrade_version-${mysql_port}
+  cp -rp $mysql_home/$mysql_version-${mysql_port}/* $mysql_home/$mysql_upgrade_version-${mysql_port}/
+  mysql_container_name=${mysql_description}-${mysql_upgrade_version}-${mysql_port}
+  docker_image=$harbor_library:$mysql_upgrade_version
+  docker run -d -p $mysql_port:$mysql_port --net $network_mode -v $mysql_conf/conf.d:/etc/mysql/conf.d -v $mysql_data:/var/lib/mysql -v $mysql_logs:/var/log/mysql -v /etc/localtime:/etc/localtime -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --name $mysql_container_name $docker_image
 fi
