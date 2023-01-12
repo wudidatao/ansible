33a01fca5ab6b2a5b75e5ab05746dfd78cef133d
wudidatao <82302925@qq.com>
2023-01-12 10:28:01 +0800

    mysql功能优化

--- mysql/docker/8.0.25/ansible_mysql_docker.sh
+++ mysql/docker/8.0.25/ansible_mysql_docker.sh
@@ -10,7 +10,7 @@ if [ $1 = "pull" ];then
   ansible $mysql_ip -m script -a "mysql_docker.sh $1 $harbor_ip $harbor_library $mysql_version"
 fi
 
-if [ $1 == "init" ] || [ $1 == "create" ] || [ $1 == "clean" ] || [ $1 == "delete" ] || [ $1 == "status" ] || [ $1 == "start" ] || [ $1 == "stop" ] || [ $1 == "restart" ];then
+if [ $1 == "clean" ] || [ $1 == "create" ] || [ $1 == "delete" ] || [ $1 == "init" ] || [ $1 == "status" ] || [ $1 == "start" ] || [ $1 == "stop" ] || [ $1 == "restart" ];then
   ansible $mysql_ip -m script -a "mysql_docker.sh $1 $harbor_ip $harbor_library $mysql_version $mysql_port $mysql_home $mysql_path $mysql_data $mysql_conf $mysql_logs $mysql_user $mysql_description $mysql_container_name $network_mode"
 fi
 
